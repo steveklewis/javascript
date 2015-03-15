dbbebc12c5e51a8ea655482603828d6f94452c43
steveklewis <lewisksteven@gmail.com>
2015-03-14 21:22:03 -0400

    Obtaining ember.js and dependencies

---
+++ emberjs/todomvc/cplibs.sh
@@ -0,0 +1,4 @@
+cp bower_components/ember/ember.js js/libs
+cp bower_components/ember-data/ember-data.js js/libs
+cp bower_components/jquery/dist/jquery.min.js js/libs
+cp bower_components/handlebars/handlebars.js js/libs
