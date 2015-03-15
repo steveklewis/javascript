76eb65c2883fddcf2462aa158ac2bca42e305172
steveklewis <lewisksteven@gmail.com>
2015-03-14 21:45:53 -0400

    Adding the first route and template

--- emberjs/todomvc/cplibs.sh
+++ emberjs/todomvc/cplibs.sh
@@ -1,4 +1,6 @@
 cp bower_components/ember/ember.js js/libs
+cp bower_components/ember/ember-template-compiler.js js/libs
 cp bower_components/ember-data/ember-data.js js/libs
 cp bower_components/jquery/dist/jquery.min.js js/libs
 cp bower_components/handlebars/handlebars.js js/libs
+
