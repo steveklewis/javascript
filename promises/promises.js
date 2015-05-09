3fd0f856e4b3f4da78dfb7943baa2147541e9d2b
steveklewis <lewisksteven@gmail.com>
2015-05-09 10:20:59 -0400

    Second example of http://www.toptal.com/javascript/javascript-promises

--- promises/promises.js
+++ promises/promises.js
@@ -4,20 +4,32 @@ function dieToss() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-console.log('1');
-var promise = new RSVP.Promise(function(fulfill, reject) {
-  var n = dieToss();
-  if (n == 6) {
-    fulfill(n);
-  } else {
-    reject(n);
-  }
-  console.log('2');
-});
+function tossASix() {
+  return new RSVP.Promise(function(fulfill, reject) {
+    var n = Math.floor(Math.random() * 6) + 1;
+    if (n === 6) {
+      fulfill(n);
+    } else {
+      reject(n);
+    }
+  });
+}
 
-promise.then(function(toss) {
-  console.log('Yay, threw a ' + toss + '.');
-}, function (toss) {
-  console.log('Oh, noes, threw a ' + toss + '.');
-});
-console.log('3');
+function logAndTossAgain(toss) {
+  console.log("Tossed a " + toss + ", need to try again.");
+  return tossASix();
+}
+
+function logSuccess(toss) {
+  console.log("Yay, managed to toss a " + toss + ".");
+}
+
+function logFailure(toss) {
+  console.log("Tossed a " + toss + ". Too bad, couldn't roll a six");
+}
+
+tossASix()
+  .then(null, logAndTossAgain)
+  .then(null, logAndTossAgain)
+  .then(logSuccess, logFailure)
+
