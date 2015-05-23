68ce72ead745f6f6b1fe7e06add4b400c53fce9d
steveklewis <lewisksteven@gmail.com>
2015-05-23 11:29:19 -0400

    Adding beach to Ember Data store

--- beaches/ember-beaches/app/routes/application.js
+++ beaches/ember-beaches/app/routes/application.js
@@ -5,10 +5,13 @@ export default Ember.Route.extend({
     openModal: function(modalName) {
       return this.render(modalName, {
         into: 'application',
-        outlet: 'modal'
+        outlet: 'modal',
+        model: {'name': '',
+                'score': 100}
       });
     },
-    closeModal: function() {
+    closeModal: function(model) {
+      this.store.createRecord('beach', model);
       return this.disconnectOutlet({
         outlet: 'modal',
         parentView: 'application'
---
+++ beaches/ember-beaches/app/routes/newbeach.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.Extend({
+  model: function() {
+    var newBeach = {'name': '',
+                    'score': 100};
+    debugger;
+    return newBeach;
+  }
+});
