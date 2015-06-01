2eedd85153ddf6413bb649366ad838952cd0c529
steveklewis <lewisksteven@gmail.com>
2015-06-01 06:28:44 -0400

    Got unit test working with jasmine-node

--- jasmine/spec/BeachSpec.js
+++ jasmine/spec/BeachSpec.js
@@ -1,12 +1,16 @@
+#!javascript
+
+var beaches = require("../src/Beach");
+
 describe("Beach", function() {
   it("should be surfable if the score is 90", function() {
-    var beach = new Beach();
-    beach.score = 90;
-    expect(beach.surfable()).toBe(true);
+    var beach1 = beaches.newBeach();
+    beach1.score = 90;
+    expect(beach1.surfable()).toBe(true);
   });
   it("should not be surfable if the score is below 70", function() {
-    var beach = new Beach();
-    beach.score = 68;
-    expect(beach.surfable()).toBe(false);
+    var beach1 = beaches.newBeach();
+    beach1.score = 68;
+    expect(beach1.surfable()).toBe(false);
   });
 });
--- jasmine/src/Beach.js
+++ jasmine/src/Beach.js
@@ -1,6 +1,12 @@
+#!javascript
+// Beach.js
+
+exports.newBeach = function() {
+  return new Beach()
+}
 function Beach() {
   this.score = 0;
-}
+};
 Beach.prototype.surfable = function() {
   return this.score > 70;
 };
