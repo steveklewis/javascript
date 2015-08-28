8675de58afaf3274273cc89a43464d20858d587c
steveklewis <lewisksteven@gmail.com>
2015-08-27 21:39:28 -0400

    First step in Meteor tutorial.

---
+++ meteor/simple-todos/simple-todos.js
@@ -0,0 +1,17 @@
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: [
+      { text: "This is task 1" },
+      { text: "This is task 2" },
+      { text: "This is task 3" }
+    ]
+  });
+}
+
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
