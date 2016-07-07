3e4da2402963ae96fe048adff3048fc36ed42e37
steveklewis <lewisksteven@gmail.com>
2016-07-06 20:32:37 -0400

    jsdom w/ config object

---
+++ jsdom/jsdom-config.js
@@ -0,0 +1,13 @@
+var jsdom = require("jsdom");
+
+jsdom.env({
+  url: "http://news.ycombinator.com/",
+  scripts: ["http://code.jquery.com/jquery.js"],
+  done: function(err, window) {
+    var $ = window.$;
+    console.log("HN Links");
+    $("td.title:not(:last) a").each(function() {
+      console.log(" -", $(this).text());
+    });
+  }
+});
